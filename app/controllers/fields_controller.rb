20469402e876d7a9648e8e1f6f411ee1285f254f
Ken-Ogasahara <radionamenanashisan3198@gmail.com>
2023-02-21 03:36:22 +0000

    [Fix]Field editing for admins only

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,22 +1,32 @@
 class FieldsController < ApplicationController
   def new
-    @field = Field.new()
+    if current_user.name == "admin"
+      @field = Field.new()
+    else
+      flash[:notice] = "フィールドは管理者ユーザーのみ操作可能です。"
+      render :index
+    end
   end
 
   def create
     @field = Field.new(field_params)
-    if  @field.save
-      flash[:notice] = "フィールドを追加しました。"
-      redirect_to fields_path
-    else
-      render :new
-    end
+      if  @field.save
+        flash[:notice] = "フィールドを追加しました。"
+        redirect_to fields_path
+      else
+        render :new
+      end
   end
 
   def destroy
-    @field = Field.find(params[:id])
-    @field.destroy
-    redirect_to fields_path
+    if current_user.name == "admin"
+      @field = Field.find(params[:id])
+      @field.destroy
+      redirect_to fields_path
+    else
+      flash[:notice] = "フィールドは管理者ユーザーのみ操作可能です。"
+      render :index
+    end
   end
 
   def index
@@ -24,18 +34,28 @@ class FieldsController < ApplicationController
   end
 
   def update
-    @field = Field.find(params[:id])
-    if  @field.update(field_params)
-      flash[:notice] = "フィールドを更新しました。"
-      redirect_to edit_field_path(@field.id)
+    if current_user.name == "admin"
+      @field = Field.find(params[:id])
+      if  @field.update(field_params)
+        flash[:notice] = "フィールドを更新しました。"
+        redirect_to edit_field_path(@field.id)
+      else
+        flash[:notice] = "フィールドの変更に失敗しました。"
+        redirect_to edit_field_path(@field.id)
+      end
     else
-      flash[:notice] = "フィールドの変更に失敗しました。"
-      redirect_to edit_field_path(@field.id)
+      flash[:notice] = "フィールドは管理者ユーザーのみ操作可能です。"
+      render :index
     end
   end
 
   def edit
-    @field = Field.find(params[:id])
+    if current_user.name == "admin"
+      @field = Field.find(params[:id])
+    else
+      flash[:notice] = "フィールドは管理者ユーザーのみ操作可能です。"
+      render :index
+    end
   end
 
   private
